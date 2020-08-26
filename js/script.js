7d36dfd87eddb0d546889719336dcd179c57038f
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-08-26 22:35:37 +0200

    app js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+/* Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome ed età.
+Stampare a schermo attraverso il for in tutte le proprietà.
+- Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome.
+- Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+Usate prima i console.log e poi provare a stampare con jQuery:
+BONUS: Provate ad utilizzare Handlebars. */
+
 //utilizzo Handlebars
 var source = document.getElementById("entry-template").innerHTML;
 var template = Handlebars.compile(source);
